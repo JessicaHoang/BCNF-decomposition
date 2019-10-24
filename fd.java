422ef5c8f24505630d7ccc26eb383d35329753eb
JessicaHoang <32215166+JessicaHoang@users.noreply.github.com>
2019-10-23 22:48:35 -0700

    Update fd.java

--- fd.java
+++ fd.java
@@ -10,7 +10,8 @@ import javax.swing.text.html.HTML.Attribute;
 public class fd {
 	
 	private relation lhs,rhs;
-
+	private List<fd> fdList;
+	private Iterator<fd> it;
 	
 	/*Constructor*/
 	public fd(relation in_lhs, relation in_rhs) {
@@ -18,6 +19,9 @@ public class fd {
 		this.lhs = in_lhs;
 		this.rhs = in_rhs;
 		
+	}
+	public fd() {
+		fdList = new ArrayList<>();
 	}
 	
 	public relation getRighthand() {
@@ -28,9 +32,9 @@ public class fd {
 		return lhs;
 	}
 	
-	public String toString() {
-		return lhs.toString()+ "->"+rhs.toString();
-	}
+//	public String toString() {
+//		return lhs.toString()+ "->"+rhs.toString();
+//	}
 	
 	public boolean BCNFviolation(relation s) {
 		relation u = lhs.union(rhs);
@@ -40,77 +44,66 @@ public class fd {
 		return false;
 	}
 	
-	
-	/*inner class*/
-	class fdList{
-		private List<fd> fdList;
-		private Iterator<fd> it;
-		
-		/*inner class constructor*/
-		public fdList() {
-			fdList = new ArrayList<>();
-		}
-		
-		public void insert(fd FD) {
-			fdList.add(FD);
-			it = fdList.iterator();
-		}
-		
-		public fd getFirst() {
-			if (!fdList.isEmpty() || fdList != null) {
-				return fdList.get(0);
-			}
-			return null;
-		}
-		
-		public fd getNext() {
-			if (it.hasNext()) {
-				return it.next();
-			}
-			return null;
-		}
-
-		/*Closure*/
-
-		public relation closure(relation r) throws CloneNotSupportedException {
-
-			relation closure = (relation) r.clone();
-			relation prevClosure = null;
-			while (true) {
-				for (fd fd : fdList) {
-					if (closure.subset(fd.getLefthand())) {
-						closure = closure.union(fd.getRighthand());
-					}
-				}
-				if (closure.equals(prevClosure)) {
-					return closure;
-				}
-				prevClosure = closure;
-			}
-		}
-		public boolean hasNext() {
-			return it.hasNext();
-		}
-
-		public Iterator<fd> getIterator() {
-			return fdList.iterator();
-		}
-
-		public List<fd> getList() {
-			return fdList;
-		}
-		public String toString() {
-			StringBuilder output = new StringBuilder();
-			for (fd rel : fdList) {
-				output.append(rel.toString() + "\n");
-			}
-			return output.toString();
-		}
-
-		public void resetIterator() {
-			it = fdList.iterator();
-		}
-		
+	public void insert(fd FD) {
+		fdList.add(FD);
+		it = fdList.iterator();
 	}
+	
+	public fd getFirst() {
+		if (!fdList.isEmpty() || fdList != null) {
+			return fdList.get(0);
+		}
+		return null;
+	}
+	
+	public fd getNext() {
+		if (it.hasNext()) {
+			return it.next();
+		}
+		return null;
+	}
+	
+	/*Closure*/
+	public relation closure(relation r) throws CloneNotSupportedException {
+
+		relation closure = (relation) r.clone();
+		relation prevClosure = null;
+		while (true) {
+			for (fd fd : fdList) {
+				if (closure.subset(fd.getLefthand())) {
+					closure = closure.union(fd.getRighthand());
+				}
+			}
+			if (closure.equals(prevClosure)) {
+				return closure;
+			}
+			prevClosure = closure;
+		}
+	}
+	
+	public boolean hasNext() {
+		return it.hasNext();
+	}
+
+	public Iterator<fd> getIterator() {
+		return fdList.iterator();
+	}
+
+	public List<fd> getList() {
+		return fdList;
+	}
+	
+	public String toString() {
+		StringBuilder output = new StringBuilder();
+		for (fd rel : fdList) {
+			output.append(rel.toString() + "\n");
+		}
+		return output.toString();
+	}
+
+	public void resetIterator() {
+		it = fdList.iterator();
+	}
+
 }
 
